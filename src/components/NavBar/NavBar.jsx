5f0c981a1c167d456df788bb5b4c4078648616d1
William Yau <122402171+WorldAdrastea@users.noreply.github.com>
2024-02-09 00:17:48 +0000

    09022024
    If applied:
    - Adds drop down feature for Games and Community options on nav bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,8 +1,11 @@
 import './NavBar.css'
 import logo from '../../imgs/mist-svgrepo-com.svg'
 import InstallButton from '../InstallButton/InstallButton'
+import { useState } from 'react'
 
 export default function NavBar() {
+    const [showDropdown, setShowDropdown] = useState(false);
+
     return (
         <div className='nav-bar'>
             <div className='nav-container'>
@@ -17,11 +20,40 @@ export default function NavBar() {
                     </h1>
                 </a>
                 <div className='navigation'>
-                    <a className='nav-item' href='/'>
+                    <a 
+                        className='nav-item' 
+                        href='/'
+                        onMouseEnter={() => setShowDropdown('games')}
+                        onMouseLeave={() => setShowDropdown(null)}
+                    >
                         Games
+                        {showDropdown === 'games' && (
+                            <div className='dropdown-content'>
+                                <a href='/'>Home</a>
+                                <a href='/'>Discovery Queue</a>
+                                <a href='/'>Wishlist</a>
+                                <a href='/'>Points shop</a>
+                                <a href='/'>News</a>
+                                <a href='/'>Stats</a>
+                            </div>
+                        )}
                     </a>
-                    <a className='nav-item' href='/'>
+                    <a 
+                        className='nav-item' 
+                        href='/'
+                        onMouseEnter={() => setShowDropdown('community')}
+                        onMouseLeave={() => setShowDropdown(null)}
+                    >
                         Community
+                        {showDropdown === 'community' && (
+                            <div className='dropdown-content'>
+                                <a href='/'>Home</a>
+                                <a href='/'>Discussion</a>
+                                <a href='/'>Workup</a>
+                                <a href='/'>Market</a>
+                                <a href='/'>Broadcasts</a>
+                            </div>
+                        )}
                     </a>
                     <a className='nav-item' href='/'>
                         About
